1ab177fa45c66687ed8180e6b4f30c620cc645c9
user <user@DESKTOP-T2KLMGU>
2021-01-12 14:02:22 +0500

    last

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const Bemor = require("./../models/Bemor");
-const Dori = require("../models/Dori");
-const Hodim = require("../models/Hodim");
-const YetBer = require("../models/YetkazibBeruvchi");
-const router = express.Router();
-
-router.get("/", async (req, res, next) => {});
